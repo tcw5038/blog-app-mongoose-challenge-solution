c8744ff2876ae944c0f56862555302043ec7ef17
Tyler Warrick <tcw5038@gmail.com>
2018-08-28 12:21:23 -0700

    Added comments for all tests

--- test/test-blog-posts.js
+++ test/test-blog-posts.js
@@ -37,34 +37,34 @@ function generateBlogPostData(){//utilizes faker to generate fake blog post info
 
 function tearDownDb(){//function that gets called to delete our test database
   console.warn('Deleting test database');
-  return mongoose.connection.dropDatabase();//mongoose function to delete the database
+  return mongoose.connection.dropDatabase();//mongoose function to delete the database/associated data files
 }
 
 describe('Blog Posts API resource', function(){
   before(function(){//before any tests have been run
-    return runServer(TEST_DATABASE_URL);
+    return runServer(TEST_DATABASE_URL);//runs the server utilizing the test_database_url specified in our config file
   });
 
-  beforeEach(function(){//meaning before each test
+  beforeEach(function(){//meaning before each test, seed the database so that there is something to test
     return seedBlogPostData();
   });
 
-  afterEach(function(){//meaning after each test
+  afterEach(function(){//meaning after each test, delete the database
     return tearDownDb();
   });
 
   after(function(){//after all tests have been run
-    return closeServer();//seems like this can be empty...why?
+    return closeServer();//does this ever need to have an argument? for example: test_database_url?
   });
 
   describe('GET endpoint', function() {
-    it('should return all existing blog posts', function() {
-      let res;
+    it('should return all existing blog posts', function() {//essentially just checks to make sure all existing blog posts are returned
+      let res;//declared here because we need to have access to it across multiple .then calls
       return chai.request(app)
         .get('/posts')
-        .then(function(_res){
-          res = _res;
-          expect(res).to.have.status(200);//checks to see if our database seeding worked
+        .then(function(_res){//using _res as to not conflict with the res declared above outside of this request
+          res = _res;//setting the declared res to the _res in the .then statement
+          expect(res).to.have.status(200);//check to make sure request has succeeded
           expect(res.body).to.have.lengthOf.at.least(1);
           return BlogPost.count();
         })
@@ -73,25 +73,25 @@ describe('Blog Posts API resource', function(){
         });
     });
 
-    it('should return blog posts with the correct fields/keys', function(){
+    it('should return blog posts with the correct fields/keys', function(){//checks to make sure all the existing blog posts are formatted correctly and have the correct keys
    
-      let resPost;
+      let resPost;//again declaring this outside so that it can be used across multiple .then statements
       return chai.request(app)
         .get('/posts')
         .then(function(res){
-          expect(res).to.have.status(200);
+          expect(res).to.have.status(200);//check to make sure request has succeeded
           expect(res).to.be.json;
           expect(res.body).to.be.a('array');
-          expect(res.body).to.have.lengthOf.at.least(1);
+          expect(res.body).to.have.lengthOf.at.least(1);//since we are seeding the database, there should always be 1 or more
 
-          res.body.forEach(function(post){
+          res.body.forEach(function(post){//for each post in the body, it should be an object and include the relevant keys
             expect(post).to.be.a('object');
             expect(post).to.include.keys('id', 'title', 'content', 'author', 'created');
           });
-          resPost = res.body[0];
-          return BlogPost.findById(resPost.id);
+          resPost = res.body[0];//sets resPost equal to the first post in the response array
+          return BlogPost.findById(resPost.id);//returns the id of the first post in the body
         })
-        .then(post => {
+        .then(post => {//ensures that we have data consistency across requests
           expect(resPost.title).to.equal(post.title);
           expect(resPost.content).to.equal(post.content);
           expect(resPost.author).to.equal(post.authorName);
@@ -101,10 +101,10 @@ describe('Blog Posts API resource', function(){
 
   describe('POST endpoint', function() {
     it('should add a new blog post', function(){
-      const newBlogPost = generateBlogPostData();//creating a new restaurant that we can send in the request
+      const newBlogPost = generateBlogPostData();//creating a new restaurant that we can send in the request...declared outside so that it can be used in second .then request for comparisons
       return chai.request(app)
         .post('/posts')
-        .send(newBlogPost)
+        .send(newBlogPost)//newly generated blog post that is sent in the post request body
         .then(function (res) {
           expect(res).to.have.status(201);//201 indicates that a resource has been created
           expect(res).to.be.json;
@@ -115,9 +115,9 @@ describe('Blog Posts API resource', function(){
           expect(res.body.author).to.equal(
             `${newBlogPost.author.firstName} ${newBlogPost.author.lastName}`);
           res.body.content.should.equal(newBlogPost.content);
-          return BlogPost.findById(res.body.id);
+          return BlogPost.findById(res.body.id);//we use the id from the response object of the newly posted blog post
         })
-        .then(function (post) {
+        .then(function (post) {//comparisons to make sure that the posted blog post is the same as the one that we intended it to be
           expect(post.title).to.equal(newBlogPost.title);
           expect(post.content).to.equal(newBlogPost.content);
           expect(post.author.firstName).to.equal(newBlogPost.author.firstName);
@@ -129,24 +129,24 @@ describe('Blog Posts API resource', function(){
 
   describe('PUT endpoint', function() {
     it('should update fields in a given blog post', function(){
-      const updatedBlogFields = {
+      const updatedBlogFields = {//manually updating some fields
         title:'Updated blog post for PUT testing purposes',
         content:'This is updated content from a test of our PUT endpoint'
       };
 
       return BlogPost
-        .findOne()
+        .findOne()//finds one post
         .then(function(post){
-          updatedBlogFields.id = post.id;
+          updatedBlogFields.id = post.id;//sets the updated post to the id of the post found
           return chai.request(app)
-            .put(`/posts/${post.id}`)
-            .send(updatedBlogFields);
+            .put(`/posts/${post.id}`)//uses the id from the findOne post so that we can update it in our test
+            .send(updatedBlogFields);//sends the updated information declared above
         })//seems like if there is a .then coming after, we don't want a semicolon here as a general rule
         .then(function(res){
-          expect(res).to.have.status(204);
-          return BlogPost.findById(updatedBlogFields.id);
+          expect(res).to.have.status(204);//successful response with no content since it is a put
+          return BlogPost.findById(updatedBlogFields.id);//returns the id
         })
-        .then(function(post){
+        .then(function(post){//comparison to make sure that the posted blog post is updated like the content we intended it to have
           expect(post.title).to.equal(updatedBlogFields.title);
           expect(post.content).to.equal(updatedBlogFields.content);
         });
@@ -155,7 +155,7 @@ describe('Blog Posts API resource', function(){
 
   describe('DELETE endpoint', function() {
     it('should delete a given blog post using its id', function(){
-      let post;
+      let post;//why not just use _post for this entire one?
       return BlogPost
         .findOne()
         .then(function(_post){
@@ -163,11 +163,11 @@ describe('Blog Posts API resource', function(){
           return chai.request(app).delete(`/posts/${post.id}`);
         })
         .then(function(res){
-          expect(res).to.have.status(204);
+          expect(res).to.have.status(204);//no content expected since this is a DELETE endpoint
           return BlogPost.findById(post.id);
         })
         .then(function(_post){
-          expect(_post).to.be.null;
+          expect(_post).to.be.null;//should be null since it shouldnt exist anymore
         });
     });
   });
